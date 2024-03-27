9610ed002d458884e98dbbb2f2aaf204d204a4d6
IshiniWickramage <147267658+IshiniWickramage@users.noreply.github.com>
2024-03-27 15:13:31 +0530

    eighth commit

--- client/src/pages/customerManagement/CustomerOverviewGeneral.jsx
+++ client/src/pages/customerManagement/CustomerOverviewGeneral.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { connect, useDispatch } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlus,
-  faSave,
-  faTrash,
-  faEdit,
-} from "@fortawesome/free-solid-svg-icons";
 import TextField from "../../components/TextField";
 
-import { updateCustomer, deleteCustomer, fetchCustomer } from "../../store/actions/customerActions";
+import { fetchCustomer } from "../../store/actions/customerActions";
 
-function CustomerOverviewGeneral({
-  customer,
-  updateCustomer,
-  deleteCustomer,
-  fetchCustomer,
-}) {
+function CustomerOverviewGeneral({ customer, fetchCustomer }) {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const [isEditing, setIsEditing] = useState(false);
   const [editedData, setEditedData] = useState({});
 
   useEffect(() => {
@@ -36,118 +23,55 @@ function CustomerOverviewGeneral({
     fetchData();
   }, [id, dispatch]);
 
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  const handleSave = () => {
-    const updatedData = { ...customer, ...editedData };
-    updateCustomer(id, updatedData);
-    setIsEditing(false);
-  };
-
-  const handleChange = (field, value) => {
-    setEditedData((prevData) => ({ ...prevData, [field]: value }));
-  };
-
-  const handleDelete = () => {
-    deleteCustomer(id);
-  };
-
   return (
     <div className="App">
-      <div className="body-part" >
-        <div >
-          
-            <label htmlFor="customerId">Customer ID</label>
-            <TextField id="customerId" type="text" value={id} disabled={true} />
-          
-
-          
-            <label htmlFor="fullName">Full Name</label>
-            <TextField
-              id="fullName"
-              type="text"
-              value={isEditing ? editedData.fullName : customer?.fullName || ""}
-              disabled={!isEditing}
-              onChange={(e) => handleChange("fullName", e.target.value)}
-            />
-          
-
-          <div >
+      <div className="body-part">
+        <div>
+          <label htmlFor="customerId">Customer ID</label>
+          <TextField id="customerId" type="text" value={id} disabled={true} />
+          <label htmlFor="fullName">Full Name</label>
+          <TextField
+            id="fullName"
+            type="text"
+            value={customer ? customer.fullName : ""}
+            disabled={true}
+          />
+          <div>
             <label htmlFor="identifier">Identifier</label>
             <TextField
               id="identifier"
               type="text"
-              value={isEditing ? editedData.identifier : customer?.identifier || ""}
-              disabled={!isEditing}
-              onChange={(e) => handleChange("identifier", e.target.value)}
+              value={customer ? customer.identifier : ""}
+              disabled={true}
             />
           </div>
-
-          <div >
+          <div>
             <label htmlFor="address">Address</label>
             <TextField
               id="address"
               type="text"
-              value={isEditing ? editedData.address : customer?.address || ""}
-              disabled={!isEditing}
-              onChange={(e) => handleChange("address", e.target.value)}
+              value={customer ? customer.address : ""}
+              disabled={true}
             />
           </div>
-
           <div>
             <label htmlFor="email">Email</label>
             <TextField
               id="email"
               type="text"
-              value={isEditing ? editedData.email : customer?.email || ""}
-              disabled={!isEditing}
-              onChange={(e) => handleChange("email", e.target.value)}
+              value={customer ? customer.email : ""}
+              disabled={true}
             />
           </div>
-
-          <div >
+          <div>
             <label htmlFor="contactNo">Contact No</label>
             <TextField
               id="contactNo"
               type="text"
-              value={isEditing ? editedData.contactNo : customer?.contactNo || ""}
-              disabled={!isEditing}
-              onChange={(e) => handleChange("contactNo", e.target.value)}
+              value={customer ? customer.contactNo : ""}
+              disabled={true}
             />
           </div>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Search"
-            style={{ marginRight: "10px", padding: "2px", borderRadius: 10 }}
-          />
-          <Link to={`/CustomerCreation`}>
-            <FontAwesomeIcon
-              icon={faPlus}
-              style={{ cursor: "pointer" }}
-            />
-          </Link>
-          {isEditing ? (
-            <FontAwesomeIcon
-              icon={faSave}
-              style={{ cursor: "pointer" }}
-              onClick={handleSave}
-            />
-          ) : (
-            <FontAwesomeIcon
-              icon={faEdit}
-              style={{ cursor: "pointer" }}
-              onClick={handleEdit}
-            />
-          )}
-          <FontAwesomeIcon
-            icon={faTrash}
-            style={{ cursor: "pointer" }}
-            onClick={handleDelete}
-          />
         </div>
       </div>
     </div>
@@ -163,8 +87,6 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchCustomer: (customerId) => dispatch(fetchCustomer(customerId)),
-    updateCustomer: (customerId, updatedData) => dispatch(updateCustomer(customerId, updatedData)),
-    deleteCustomer: (customerId) => dispatch(deleteCustomer(customerId)),
   };
 };
 
