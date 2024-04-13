bd6ab74e7fb924cd89edb3e982c4b90fddaad3aa
IshiniWickramage <147267658+IshiniWickramage@users.noreply.github.com>
2024-04-13 19:42:50 +0530

    Update ReservationList.jsx

--- client/src/pages/reservationOverviewPart/ReservationList.jsx
+++ client/src/pages/reservationOverviewPart/ReservationList.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchReservations, deleteReservation } from "../../store/actions/ReservationAction";
-import { Row, Col, Button, Form, InputGroup } from "react-bootstrap";
+import { Row, Button, Form, InputGroup } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH, faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import TitleActionBar from "../../components/TitleActionsBar";
+import ReservationGroupTable from "../../components/table/DataTableComponent";
+import { DeleteConfirmModel } from "../../components/DeleteConfirmModel";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const ReservationList = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const reservations = useSelector((state) => state.reservation.reservations);
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
   const [isFiltered, setIsFiltered] = useState(false);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [currentPage, setCurrentPage] = useState(0);
+  const [perPage, setPerPage] = useState(5);
 
   useEffect(() => {
-    dispatch(fetchReservations());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (reservations.length > 0) {
+    dispatch(fetchReservations()).then(() => {
       setLoading(false);
-    }
-  }, [reservations]);
+    });
+  }, [dispatch]);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -31,21 +41,108 @@ const ReservationList = () => {
   const clearFilter = () => {
     setSearchTerm("");
     setIsFiltered(false);
+    setCurrentPage(0);
   };
 
-  const handleDelete = (id) => {
-    dispatch(deleteReservation(id));
+  const handleDelete = () => {
+    setShowConfirmation(true);
   };
+
+  const confirmDelete = () => {
+    if (selectedRows.length === 1) {
+      try {
+        dispatch(deleteReservation(selectedRows[0]?.reservationID));
+        toast.success("Record Successfully deleted!");
+      } catch (error) {
+        toast.error("Error deleting row. Please try again.");
+      } finally {
+        setShowConfirmation(false);
+      }
+    }
+  };
+
+  const handleEdit = () => {
+    if (selectedRows.length === 1) {
+      // Perform edit action here, such as navigating to an edit page
+      console.log("Edit action");
+    }
+  };
+
+  const handleDetails = () => {
+    if (selectedRows.length === 1) {
+      // Perform details action here, such as displaying more information
+      console.log("Details action");
+    }
+  };
+
+  const columns = [
+    {
+      name: "",
+      cell: (row) => (
+        <div className="cell-actions">
+          <span
+            className="ellipsis tree-dots"
+            onClick={(e) => handleCellClick(e, row)}
+          >
+            <FontAwesomeIcon icon={faEllipsisH} />
+          </span>
+        </div>
+      ),
+    },
+    {
+      name: "Reservation ID",
+      selector: (row) => row.reservationID,
+      sortable: true,
+    },
+    {
+      name: "Customer ID",
+      selector: (row) => row.customerID,
+      sortable: true,
+    },
+    {
+      name: "Time Slot",
+      selector: (row) => `${row.time1} - ${row.time2}`,
+      sortable: true,
+    },
+    {
+      name: "Date",
+      selector: (row) => row.date,
+      sortable: true,
+    },
+  ];
+
+  const handleCellClick = (e, row) => {
+    e.preventDefault();
+    setContextMenuPosition({ x: e.clientX, y: e.clientY });
+    setMenuVisible(true);
+  };
+
+  const customContextMenu = menuVisible && (
+    <div
+      className="styled-menu"
+      style={{ top: contextMenuPosition.y, left: contextMenuPosition.x }}
+    >
+      <div className="menu-item" onClick={handleEdit}>
+        Edit
+      </div>
+      <div className="menu-item" onClick={handleDetails}>
+        Details
+      </div>
+      <div className="menu-item" onClick={handleDelete}>
+        Delete
+      </div>
+    </div>
+  );
 
   return (
     <div className="mb-5 mx-2">
       <TitleActionBar
         Title={"Reservation List"}
-        deleteDisabled={true}
         PlusAction={() => {}}
-        EditAction={() => {}}
+        EditAction={handleEdit}
         SaveAction={() => {}}
-        DeleteAction={() => {}}
+        DeleteAction={handleDelete}
+        deleteDisabled={selectedRows.length !== 1}
       />
 
       <Row>
@@ -60,7 +157,7 @@ const ReservationList = () => {
             />
             {isFiltered ? (
               <Button variant="primary" className="form-btn" onClick={clearFilter}>
-                <FontAwesomeIcon icon={faXmark} size="lg" />
+                <FontAwesomeIcon icon={faXmark} />
               </Button>
             ) : (
               <Button variant="primary" className="form-btn" onClick={() => {}}>
@@ -72,33 +169,31 @@ const ReservationList = () => {
       </Row>
 
       <div className="table-responsive">
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Reservation ID</th>
-              <th>Customer ID</th>
-              <th>Time Slot</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loading ? (
-              <tr>
-                <td colSpan="4">Loading reservations...</td>
-              </tr>
-            ) : (
-              reservations.map((reservation) => (
-                <tr key={reservation.reservationID}>
-                  <td>{reservation.reservationID}</td>
-                  <td>{reservation.customerID}</td>
-                  <td>{`${reservation.time1} - ${reservation.time2}`}</td> {/* Derive time slot from time1 and time2 */}
-                  <td>{reservation.date}</td>
-                </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+      <ReservationGroupTable
+          selectableRows={true}
+          selectableRowsSingle={true}
+          setPerPage={setPerPage}
+          setCurrentPage={setCurrentPage}
+          setSelectedRows={setSelectedRows}
+          setMenuVisible={setMenuVisible}
+          paginatedData={reservations}
+          totalItems={reservations.length}
+          currentPage={currentPage}
+          perPage={perPage}
+          columns={columns}
+        />
       </div>
+
+      {customContextMenu}
+
+      <DeleteConfirmModel
+        show={showConfirmation}
+        close={() => setShowConfirmation(false)}
+        title={"Warning"}
+        message={"The selected Reservation will be deleted. Do you wish to continue?"}
+        type={"Yes"}
+        action={confirmDelete}
+      />
     </div>
   );
 };
