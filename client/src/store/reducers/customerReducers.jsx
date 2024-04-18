3831baa7b63576c2e2826009a879d2771d1073f9
IshiniWickramage <147267658+IshiniWickramage@users.noreply.github.com>
2024-04-18 09:19:10 +0530

    Added missed redux parts for error handling

--- client/src/store/reducers/customerReducers.jsx
+++ client/src/store/reducers/customerReducers.jsx
@@ -4,6 +4,7 @@ const initialState = {
   customers: [],
   customer:{},
   customer: null,
+  error: null,
 };
 
 const customerReducer = (state = initialState, action) => {
@@ -12,35 +13,66 @@ const customerReducer = (state = initialState, action) => {
       return {
         ...state,
         customers: action.payload,
+        error: null,
+      };
+    case 'FETCH_CUSTOMERS_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
       };
     case 'FETCH_CUSTOMER_SUCCESS':
       return {
         ...state,
         customer: action.payload,
+        error: null,
+      };
+    case 'FETCH_CUSTOMER_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
       };
     case 'CREATE_CUSTOMER_SUCCESS':
       return {
         ...state,
         customers: [...state.customers, action.payload],
+        error: null,
       };
-      case 'UPDATE_CUSTOMER_SUCCESS':
-        return {
-          ...state,
-          customers: state.customers.map(customer =>
-            customer.id === action.payload.id ? action.payload : customer
-          ),
-          customer: action.payload,
-        };
-      case 'DELETE_CUSTOMER_SUCCESS':
-        return {
-          ...state,
-          customers: state.customers.filter(customer => customer.id !== action.payload.id),
-          customer: null,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  
+    case 'CREATE_CUSTOMER_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case 'UPDATE_CUSTOMER_SUCCESS':
+      return {
+        ...state,
+        customers: state.customers.map((customer) =>
+          customer.id === action.payload.id ? action.payload : customer
+        ),
+        customer: action.payload,
+        error: null,
+      };
+    case 'UPDATE_CUSTOMER_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case 'DELETE_CUSTOMER_SUCCESS':
+      return {
+        ...state,
+        customers: state.customers.filter(
+          (customer) => customer.id !== action.payload.id
+        ),
+        customer: null,
+        error: null,
+      };
+    case 'DELETE_CUSTOMER_FAILURE':
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 export default customerReducer;
