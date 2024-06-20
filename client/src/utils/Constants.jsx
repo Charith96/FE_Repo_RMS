efed62e56bdc1d1800fec82a1a1674d12109a5e7
Supun Jayathilaka <147173476+Supun-Ishara@users.noreply.github.com>
2024-06-21 01:14:59 +0530

    Integrated frontend and backend.

--- client/src/utils/Constants.jsx
+++ client/src/utils/Constants.jsx
@@ -3,17 +3,14 @@ export const RESERVATION_URL='/reservations';
 export const RESERVATION_GROUP='/groupcreation';
 export const RESERVATION_ITEM='/itemcreation';
 export const TIME_SLOT='/timeslotsitem';
-export const COMPANY_DETAILS='/companies';
-export const COUNTRY_URL='/countries';
-export const CURRENCY_URL='/currencies';
 export const USER_URL = '/users';
 export const ROLE_URL = '/Roles';
-<<<<<<< HEAD
- //const BASE_URL = 'https://localhost:7291/api/Company';
 
-//  export const COMPANY_DETAILS='/Company';
-//  export const COUNTRY_URL='/Country';
-//  export const CURRENCY_URL='/Currency';
-=======
+/* Company */
+ export const COMPANY_DETAILS='/Company';
+ export const COUNTRY_URL='/Country';
+ export const CURRENCY_URL='/Currency';
+ /* Company */
+
 export const ADMIN_URL = '/admincreation';
->>>>>>> origin/integration
+
